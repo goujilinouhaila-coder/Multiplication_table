6857a5e409436750f5356a447b110b53b9cd9e24
Assiab2707 <assiab2707@yahoo.com>
2021-03-20 00:25:10 +0100

    Creation of graph class with all members

--- Project/package_table/multiplication_table/process_math/Graph.py
+++ Project/package_table/multiplication_table/process_math/Graph.py
@@ -1,1 +1,22 @@
+import numpy as np 
+import math
 
+class Graph: 
+
+    def __init__(self, table_number, modulo_number):
+        self.N = table_number
+        self.mod = modulo_number
+        self.M = np.full((self.N,self.N), 0)
+
+    def print_matrix(self):
+        print(self.M)
+
+    def add_edge(self, i, j):
+        self.M[i,j] = 1
+        self.M[j,i] = 1
+
+    def print_graph(self):
+        for i in range(self.N):
+            for j in range(i, self.N):
+                if (self.M[i,j]==1):
+                    print(i, "<--->", j) 
