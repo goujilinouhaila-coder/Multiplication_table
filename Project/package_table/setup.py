b4006b69425bc00506e07ed0b55aefee0cb38f4e
Assiab2707 <78488806+Assiab2707@users.noreply.github.com>
2021-04-21 17:05:20 +0200

    Update setup.py

--- Project/package_table/setup.py
+++ Project/package_table/setup.py
@@ -4,7 +4,7 @@ from multiplication_table import __version__ as current_version
 setup(
   name='multiplication_table',
   version=current_version,
-  description='Visualisation of table modular multiplication',
+  description='Visualisation of the modular multiplication table',
   url='https://github.com/goujilinouhaila-coder/Multiplication_table.git',
   author='SOBOLAK Valérian, GOUJILI Nouhaila, SENE Assane, BERRANDOU Assia',
   author_email= "valerian.sobolak@etu.umontpellier.fr, nouhaila.goujili@etu.umontpellier.fr, assane.sene@etu.umontpellier.fr, assia.berrandou@etu.umontpellier.fr,",
