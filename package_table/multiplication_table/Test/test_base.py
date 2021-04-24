27c5475321aa541dd489e1a481c1083526d6c24f
vsobolak <78488887+vsobolak@users.noreply.github.com>
2021-04-25 00:39:11 +0200

    Update test_base.py

--- package_table/multiplication_table/Test/test_base.py
+++ package_table/multiplication_table/Test/test_base.py
@@ -1,5 +1,6 @@
 from multiplication_table.process_vis.base_vis import coord, angle_tab, circle, dot, name_vertices
 from multiplication_table.process_math.Graph import Graph
+import tkinter
 from tkinter import Tk, Canvas
 
 
